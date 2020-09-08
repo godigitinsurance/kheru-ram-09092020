1bd88d572c4e022a0db998a32edec4d964eb48d5
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-09-08 14:36:41 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Kheru Ram");
+$("#policyNumber").text("GA77664030");
+$("#startDate").text("09-Sep-2020");
+$("#endDate").text("08-Sep-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08CQ4262");
+$("#make").text("HERO MOTOCORP");
+$("#model").text("MAESTRO");
+$("#vehicalSubType").text("ELECTRIC START");
+$("#engineNo").text("JF32AADGM01399");
+$("#chassis").text("MBLJF32ABDGM01622");
